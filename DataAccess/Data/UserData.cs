f1fcd4697c07a8a28d7848c88f6e3c1b83f24ca8
Qusai Al-Ramadhani <98ayhaythm@gmail.com>
2026-02-20 03:41:32 +0300

    refactor: changed the logic of storing user password to using hash and salt enhancing security

--- DataAccess/Data/UserData.cs
+++ DataAccess/Data/UserData.cs
@@ -1,192 +1,270 @@
-﻿using Contracts.DTOs;
-using DataAccess.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Contracts.DTOs;
+using DataAccess.Common;
 
 namespace DataAccess.Data
 {
-    public class UserData
+    public static class UserData
     {
-        public static UserDto GetUserByUserId(int userId)
-        {
-            UserDto user = null;
-
-            using (SqlConnection connection = DbConnectionFactory.CreateConnection())
-            {
-                string query = @"
-                                SELECT 
-	                                UserID,
-	                                PersonID,
-	                                UserName,
-	                                Password,
-	                                IsActive 
-                                FROM Users WHERE UserID = @UserId;
-                                ";
-                using (SqlCommand command = new SqlCommand(query, connection))
-                {
-                    command.Parameters.Add("@UserId", System.Data.SqlDbType.Int).Value = userId;
-
-                    try
-                    {
-                        connection.Open();
-                        using (SqlDataReader reader = command.ExecuteReader())
-                        {
-                            if (!reader.Read())
-                            {
-                                return null;
-                            }
-                            user = new UserDto
-                            {
-                                UserId = reader.GetInt32(reader.GetOrdinal("UserID")),
-                                PersonId = reader.GetInt32(reader.GetOrdinal("PersonID")),
-                                UserName = reader.GetString(reader.GetOrdinal("UserName")),
-                                Password = reader.GetString(reader.GetOrdinal("Password")),
-                                IsActive = reader.GetBoolean(reader.GetOrdinal("IsActive"))
-                            };
-                        }
-                    }
-                    catch (Exception)
-                    {
-
-                        throw;
-                    }
-                }
-            }
-
-                return user;
-        }
 
         public static int AddNewUser(UserDto user)
         {
             int userId = -1;
+            const string query = @"
+                INSERT INTO dbo.Users (PersonID, UserName, PasswordHash, PasswordSalt, IsActive)
+                VALUES (@PersonID, @UserName, @PasswordHash, @PasswordSalt, @IsActive);
 
-            string query = @"
-                        INSERT INTO Users (PersonID, UserName, Password, IsActive)
-                        VALUES (@PersonID, @UserName, @Password, @IsActive);
-
-                        SELECT CAST(SCOPE_IDENTITY() AS INT);
-                    ";
+                SELECT SCOPE_IDENTITY();
+            ";
 
             using (SqlConnection connection = DbConnectionFactory.CreateConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
             {
-                using (SqlCommand command = new SqlCommand(query, connection))
+                command.Parameters.Add("@PersonID", SqlDbType.Int).Value = user.PersonId;
+                command.Parameters.Add("@UserName", SqlDbType.NVarChar, 20).Value = user.UserName;
+                command.Parameters.Add("@PasswordHash", SqlDbType.NVarChar, 200).Value = user.PasswordHash;
+                command.Parameters.Add("@PasswordSalt", SqlDbType.NVarChar, 200).Value = user.PasswordSalt;
+                command.Parameters.Add("@IsActive", SqlDbType.Bit).Value = user.IsActive;
+
+                connection.Open();
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(), out int insertedId))
                 {
-                    command.Parameters.Add("@PersonID", System.Data.SqlDbType.Int).Value = user.PersonId;
-                    command.Parameters.Add("@UserName", System.Data.SqlDbType.NVarChar, 20).Value = user.UserName;
-                    command.Parameters.Add("@Password", System.Data.SqlDbType.NVarChar, 20).Value = user.Password;
-                    command.Parameters.Add("@IsActive", System.Data.SqlDbType.Bit).Value = user.IsActive;
-
-                    connection.Open();
-                    object result = command.ExecuteScalar();
-
-                    if (result != null && result != DBNull.Value)
-                    {
-                        userId = Convert.ToInt32(result);
-                    }
+                    userId = insertedId;
                 }
             }
-
             return userId;
         }
 
-        public static bool UpdateUser(UserDto user)
+        public static UserDto GetUserByUserId(int userId)
         {
-            return false;
-        }
-
-        public static bool DeleteUser(int userId)
-        {
-            return false;
-        }
-
-        public static bool IsUserExistsByPersonId(int personId)
-        {
-            string query = @"
-                        SELECT CASE 
-                            WHEN EXISTS (SELECT 1 FROM Users WHERE PersonID = @PersonID) 
-                            THEN 1 ELSE 0 
-                        END;
-                    ";
-            using (SqlConnection connection = DbConnectionFactory.CreateConnection())
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                command.Parameters.Add("@PersonID", System.Data.SqlDbType.Int).Value = personId;
-
-                connection.Open();
-                int result = (int)command.ExecuteScalar();
-
-                return result == 1;
-            }
-        }
-
-        public static bool IsUserExistsByUserId(int userId)
-        {
-            string query = @"
-                        SELECT CASE 
-                            WHEN EXISTS (SELECT 1 FROM Users WHERE UserID = @UserID) 
-                            THEN 1 ELSE 0 
-                        END;
-                    ";
+            const string query = @"
+                SELECT UserID, PersonID, UserName, IsActive
+                FROM dbo.Users
+                WHERE UserID = @UserID;
+            ";
 
             using (SqlConnection connection = DbConnectionFactory.CreateConnection())
             using (SqlCommand command = new SqlCommand(query, connection))
             {
-                command.Parameters.Add("@UserID", System.Data.SqlDbType.Int).Value = userId;
+                command.Parameters.Add("@UserID", SqlDbType.Int).Value = userId;
 
                 connection.Open();
-                int result = (int)command.ExecuteScalar();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (!reader.Read())
+                        return null;
 
-                return result == 1;
+                    return new UserDto
+                    {
+                        UserId = (int)reader["UserID"],
+                        PersonId = (int)reader["PersonID"],
+                        UserName = (string)reader["UserName"],
+                        IsActive = (bool)reader["IsActive"]
+                    };
+                }
             }
         }
 
         public static List<UserDto> GetAllUsers()
         {
+            const string query = @"
+                SELECT UserID, PersonID, UserName, IsActive
+                FROM dbo.Users;
+            ";
+
             List<UserDto> users = new List<UserDto>();
 
-            string query = @"
-                            SELECT 
-                                UserID, PersonID, UserName, IsActive 
-                            FROM Users;
-                            ";
             using (SqlConnection connection = DbConnectionFactory.CreateConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
             {
-                using (SqlCommand command = new SqlCommand(query, connection))
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
                 {
-                    try
-                    {
-                        connection.Open();
+                    int ordUserId = reader.GetOrdinal("UserID");
+                    int ordPersonId = reader.GetOrdinal("PersonID");
+                    int ordUserName = reader.GetOrdinal("UserName");
+                    int ordIsActive = reader.GetOrdinal("IsActive");
 
-                        using (SqlDataReader reader = command.ExecuteReader())
+                    while (reader.Read())
+                    {
+                        users.Add(new UserDto
                         {
-                            while (reader.Read())
-                            {
-                                users.Add(new UserDto
-                                {
-                                    UserId = reader.GetInt32(reader.GetOrdinal("UserID")),
-                                    PersonId = reader.GetInt32(reader.GetOrdinal("PersonID")),
-                                    UserName = reader.GetString(reader.GetOrdinal("UserName")),
-                                    IsActive = reader.GetBoolean(reader.GetOrdinal("IsActive")),
-                                    Password = null
-                                });
-                            }
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-
-                        throw;
+                            UserId = reader.GetInt32(ordUserId),
+                            PersonId = reader.GetInt32(ordPersonId),
+                            UserName = reader.GetString(ordUserName),
+                            IsActive = reader.GetBoolean(ordIsActive)
+                        });
                     }
                 }
             }
 
-                return users;
+            return users;
+        }
+
+        public static (string Hash, string Salt) GetPasswordData(string userName)
+        {
+            const string query = @"
+                SELECT PasswordHash, PasswordSalt
+                FROM dbo.Users
+                WHERE UserName = @UserName;
+            ";
+
+            using (SqlConnection connection = DbConnectionFactory.CreateConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.Add("@UserName", SqlDbType.NVarChar, 20).Value = userName;
+
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (!reader.Read())
+                        return (null, null);
+
+                    return (
+                        reader["PasswordHash"].ToString(),
+                        reader["PasswordSalt"].ToString()
+                    );
+                }
+            }
+        }
+
+        public static UserDto GetUserByUserName(string userName)
+        {
+            const string query = @"
+                SELECT UserID, PersonID, UserName, IsActive
+                FROM dbo.Users
+                WHERE UserName = @UserName;
+            ";
+
+            using (SqlConnection connection = DbConnectionFactory.CreateConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.Add("@UserName", SqlDbType.NVarChar, 20).Value = userName;
+
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (!reader.Read())
+                        return null;
+
+                    return new UserDto
+                    {
+                        UserId = (int)reader["UserID"],
+                        PersonId = (int)reader["PersonID"],
+                        UserName = (string)reader["UserName"],
+                        IsActive = (bool)reader["IsActive"]
+                    };
+                }
+            }
+        }
+
+        public static bool UpdateUser(UserDto user)
+        {
+            int rowsAffected = 0;
+            const string query = @"
+                UPDATE dbo.Users
+                SET PersonID = @PersonID,
+                    UserName = @UserName,
+                    IsActive = @IsActive
+                WHERE UserID = @UserID;
+            ";
+
+            using (SqlConnection connection = DbConnectionFactory.CreateConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.Add("@PersonID", SqlDbType.Int).Value = user.PersonId;
+                command.Parameters.Add("@UserName", SqlDbType.NVarChar, 20).Value = user.UserName;
+                command.Parameters.Add("@IsActive", SqlDbType.Bit).Value = user.IsActive;
+                command.Parameters.Add("@UserID", SqlDbType.Int).Value = user.UserId;
+
+                connection.Open();
+                rowsAffected =  command.ExecuteNonQuery();
+            }
+            return rowsAffected > 0;
+        }
+
+        public static bool UpdateUserPassword(int userId, string newHash, string newSalt)
+        {
+            int rowsAffected = 0;
+            const string query = @"
+                UPDATE dbo.Users
+                SET PasswordHash = @Hash,
+                    PasswordSalt = @Salt
+                WHERE UserID = @UserID;
+            ";
+
+            using (SqlConnection connection = DbConnectionFactory.CreateConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.Add("@Hash", SqlDbType.NVarChar, 200).Value = newHash;
+                command.Parameters.Add("@Salt", SqlDbType.NVarChar, 200).Value = newSalt;
+                command.Parameters.Add("@UserID", SqlDbType.Int).Value = userId;
+
+                connection.Open();
+                rowsAffected =  command.ExecuteNonQuery();
+            }
+            return rowsAffected > 0;
+        }
+
+        public static bool DeleteUser(int userId)
+        {
+            int rowsAffected = 0;
+            const string query = "DELETE FROM dbo.Users WHERE UserID = @UserID;";
+
+            using (SqlConnection connection = DbConnectionFactory.CreateConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.Add("@UserID", SqlDbType.Int).Value = userId;
+
+                connection.Open();
+                rowsAffected =  command.ExecuteNonQuery();
+            }
+            return rowsAffected > 0;
+        }
+
+        public static bool IsUserExistsByPersonId(int personId)
+        {
+            bool isFound = false;
+            const string query = @"
+                SELECT 1
+                FROM dbo.Users
+                WHERE PersonID = @PersonID;
+            ";
+
+            using (SqlConnection connection = DbConnectionFactory.CreateConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.Add("@PersonID", SqlDbType.Int).Value = personId;
+
+                connection.Open();
+                isFound =  command.ExecuteScalar() != null;
+            }
+            return isFound;
+        }
+
+        public static bool IsUserExistsByUserId(int userId)
+        {
+            bool isFound = false;
+            const string query = @"
+                SELECT 1
+                FROM dbo.Users
+                WHERE UserID = @UserID;
+            ";
+
+            using (SqlConnection connection = DbConnectionFactory.CreateConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.Add("@UserID", SqlDbType.Int).Value = userId;
+
+                connection.Open();
+                isFound =  command.ExecuteScalar() != null;
+            }
+            return isFound;
         }
     }
-
-    
 }
